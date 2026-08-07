4fbe9ca9f41c319c7c44ae23ecaac74dde96c75e
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-08-07 10:24:14 +0530

    nestedif method

---
+++ Introducation/Decision Making/nestedif.java
@@ -0,0 +1,33 @@
+/*0-3 > child
+4-12 > kid
+13-19 > teenager
+20-59 > adult
+60- > senior citizen*/
+
+import java.util.Scanner;
+class nestedifage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your age");
+        int a = sc.nextInt();
+        if (a<=3){
+            System.out.println("child");
+        }
+        else{
+            if(a<=12 && a>=4){
+             System.out.println("kid");  
+        }
+        }
+        if (a>=13 && a<=19){
+            System.out.println("teenager"); 
+        }
+        if (a>=20 && a<=59){
+            System.out.println("adult");
+        }
+        if(a>=60){
+            System.out.println("senior citizen");
+        }
+    }
+}
+      
+
