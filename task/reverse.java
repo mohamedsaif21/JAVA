22474d5a8d11a632a09f6d057bb363af98754668
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-08-10 10:16:43 +0530

    reverse the number

--- task/reverse.java
+++ task/reverse.java
@@ -6,8 +6,12 @@ public class reverse {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n:");
         int num = sc.nextInt();
-        int rev = 0;
-        int temp = num; 
-        
+        int rev = 0; 
+        while(num>0){
+            int rem = num % 10 ;
+            rev = rev*10+rem ;
+            num = num/10;       
+}
+       System.out.println("The reverse of  is " +rev);
 }
 }
