b5ccc0e47779ef813e99d20054176ab51deb2c3a
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-08-12 12:35:41 +0530

    hollowpatter.java

---
+++ pattern/hollowpattern.java
@@ -0,0 +1,32 @@
+package pattern;
+
+import java.util.*;
+public class hollowpattern {
+
+    public static void main(String []arge){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+/*
+*****
+*   *
+*   *
+*   *
+*****
+*/
+        for (int i =1; i<=n; i++){
+            for (int j=1; j<=n; j++){
+            if(i==1 || i==n || j==1 || j==n){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+            }
+         System.out.println();
+        }
+       
+    }
+    
+}
