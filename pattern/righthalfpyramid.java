615f3880e0cb36673d0124aafc5b745b64883267
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-08-12 12:22:08 +0530

    change the class setting

--- pattern/righthalfpyramid.java
+++ pattern/righthalfpyramid.java
@@ -13,7 +13,15 @@ public class righthalfpyramid {
 **       <----- Row=col(2)
 ***      <----- Row=col(3)
 ****     <----- Row=col(4)
-*/
+*/  
+          for(int row = 1; row<=n; row++){
+
+            for(int col=1 ; col<=row; col++){
+
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
         
 /*
 ****  <----- col=row(4)
