be266541b9af8f8f96010543f3e6b01df10b7caf
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-08-13 10:47:21 +0530

    crt the code in the nameinput file

--- array/nameinput.java
+++ array/nameinput.java
@@ -3,22 +3,28 @@ package array;
 import java.util.*;
 public class nameinput {
 
-    static void printArray(String[]args){
+    static void printArray(String[] names){
 
+        System.out.println("names:");
+        for(int i=0; i<names.length; i++){
+            System.out.println(names[i]+" ");
+        }
+    }
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
         int size = sc.nextInt();
+        //Declaring array
         String names[] = new String[size];
         System.out.println("Enter the names:");
-
+        //user input for array
         for(int i=0; i<size; i++){
-            names[i]=sc.next();
-            
+            names[i]=sc.next();    
         }
+        //calling method to print array
         printArray(names);
  
     }
     
 }
-}
-
