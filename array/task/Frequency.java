e5f0ddcd08c7c5e4133e0248268c3385f1501478
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-08-14 11:16:41 +0530

    Frequency

---
+++ array/task/Frequency.java
@@ -0,0 +1,26 @@
+package array.task;
+
+public class Frequency {
+
+    public static void main(String [] args){
+
+       int nums [] ={2,2,3,4,4,2};
+       boolean visited [] = new boolean[nums.length];
+       int len = nums.length;
+       for(int i=0;i<len;i++){
+        if(visited[i]==false){
+            int count = 1;
+            for(int j=i+1;j<len;j++){
+                if(nums[i]==nums[j]){
+                    visited[j]=true;
+                    count++;
+                }
+            }
+            System.out.println(nums[i]+" -> " +count);
+
+        }
+       }
+
+    }
+    
+}
