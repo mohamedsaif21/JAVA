f658d928e1a5dbe634018892d539daca17f31f15
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-08-31 20:23:55 +0530

    learing task folder

---
+++ Learningtask/Scannerclass.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Scannerclass{
+public static void main (String []args){
+
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter your name:")
+    String a = sc.nextLine();
+
+    System.out.println(a);
+}
+}
