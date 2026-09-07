c877a1e463b2d4708fbd79d0b8fedda71a3df3ec
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-07 19:32:37 +0530

    datatype learn

---
+++ Javacourse/Datatype.java
@@ -0,0 +1,22 @@
+package Javacourse;
+
+public class Datatype {
+
+    public static void main(String[] args) {
+        
+        int num1 = 2;
+        byte by =127;
+        short sh = 558;
+        long l = 5854l;
+
+
+        float f = 5.8f;
+        double d = 5.678;
+
+        char c = 'j';
+
+        boolean b =true;
+
+    }
+    
+}
