98e63c68bbb6dc4dbaa8209f083c54376ae41132
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 19:23:16 +0530

    while loop

---
+++ Javacourse/statments/whileloop.java
@@ -0,0 +1,19 @@
+package Javacourse.statments;
+
+public class whileloop {
+
+    public static void main(String[] args) {
+        
+        // loop -> while , do while , for
+        //print it 100 times - condition 
+        int i = 1; // i -> variable name
+        while (i<=4) 
+        {
+            System.out.println("Hello i am Loop " + i);
+            i++;   
+        }
+        System.out.println("Bye" + i);
+        
+    } 
+    
+}
