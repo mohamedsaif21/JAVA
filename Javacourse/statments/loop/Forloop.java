4f0f60a6f907232e03ea4ab8576caca980439983
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 19:49:50 +0530

    for loop

--- Javacourse/statments/loop/Forloop.java
+++ Javacourse/statments/loop/Forloop.java
@@ -1,5 +1,18 @@
 package Javacourse.statments.loop;
 
 public class Forloop {
+
+    public static void main(String[] args) {
+        //int i = 5; //Initialization
+        
+        for(int i=0;i<=3;i++) //(Initialization;Condition;Increment)
+        //while (i<=4)  //Condition
+        {
+            System.out.println("Hello i am Loop " + i);
+
+            //i++; //Increment
+             
+        }
+    }
     
 }
