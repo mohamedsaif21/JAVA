bfa5269a7cf43c3b272024b7d139833b793ec343
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 19:39:56 +0530

    do while loop

---
+++ Javacourse/statments/loop/DoWhileLoop.java
@@ -0,0 +1,16 @@
+package Javacourse.statments.loop;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int i = 5;
+         
+        do
+        {
+            System.out.println("Hello i am Loop " + i);
+            i ++;
+             
+        }while (i<=4);
+    }
+    
+}
