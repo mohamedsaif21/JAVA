1ab0f80ed47fba49c86d0c12f92214bc5e2fd368
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 06:26:09 +0530

    Typecasting

---
+++ Javacourse/Typecasting.java
@@ -0,0 +1,30 @@
+package Javacourse;
+
+class Typecasting {
+
+  public static void main(String[] args) {
+    
+    //byte b = 125;
+    int a = 12;
+    int b = 257;
+    byte k = (byte)a; //typecasting
+    byte j = (byte)b;
+
+ 
+    float f = 5.6f;
+    int h = (int)f; //typecasting
+
+    //type promotion
+    byte c = 10;
+    byte d =30;
+
+    int result = c * d;
+
+  System.out.println(k);
+  System.out.println(j);
+  System.out.println(h);
+  System.out.println(result);
+
+  }
+    
+}
