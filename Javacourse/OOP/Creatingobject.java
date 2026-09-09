379c16f43a3eb08d7f532f07bed8b9bf0d4be10c
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 20:53:42 +0530

    creating the object

---
+++ Javacourse/OOP/Creatingobject.java
@@ -0,0 +1,34 @@
+package Javacourse.OOP;
+
+//TASK : Create a calculator
+
+class Calculator {
+    // every object as 2 things Properties and Methods
+    int a;
+    public int add(int a1, int a2){ //Methods
+        //System.out.println("In Add");
+        int r = a1 + a2;
+        return r;
+    }
+}
+public class Creatingobject {
+
+    public static void main(String[] args) {
+        
+        int a1 =10;
+        int a2 =20; // a and b are Primitive Variables
+        //int result = a+b; = 30
+        
+        Calculator calc = new Calculator();  // Reference Variable
+        int result = calc.add(a1,a2);
+
+        System.out.println(result);
+
+    }
+    
+}
+
+//
+// Object - Properties and Behaviors
+
+// Class JVM create it  
