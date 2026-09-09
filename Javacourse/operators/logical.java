f5dc158dcfe6cddfdf02b013efe908dea3912ded
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 11:54:50 +0530

    logical

--- Javacourse/operators/logical.java
+++ Javacourse/operators/logical.java
@@ -4,7 +4,18 @@ public class logical {
 
     public static void main(String[] args) {
         
-        
+        int x = 7;
+        int y = 5;
+        int a = 5;
+        int b = 9;
+
+        boolean result = x > y && a < b ;
+        boolean result1 = x > y || a > b || a < 1;
+        boolean result2 =  a > b;
+
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(!result2);
     }
     
 }
