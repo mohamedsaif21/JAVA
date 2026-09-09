95acc712705d051a0ca22bcff26c4ea30f744fc5
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 20:08:42 +0530

    for loop

--- Javacourse/statments/loop/Forloop.java
+++ Javacourse/statments/loop/Forloop.java
@@ -3,16 +3,27 @@ package Javacourse.statments.loop;
 public class Forloop {
 
     public static void main(String[] args) {
-        //int i = 5; //Initialization
+        /*int i = 5; //Init ialization
         
         for(int i=0;i<=3;i++) //(Initialization;Condition;Increment)
         //while (i<=4)  //Condition
         {
             System.out.println("Hello i am Loop " + i);
 
-            //i++; //Increment
+            ++; //Increment */
+//task: Calendar
+        //nested for loop
+        for (int i=1;i<=5;i++) {
+
+            System.out.println("Day " + i);
+            //we are using the inner loop
+            for(int j=1;j<=9;j++){
+
+                System.out.println("  " + (j+8) + " - " + (j+9)); // Concatenation
+                
+            }
+            
+        }
              
         }
     }
-    
-}
