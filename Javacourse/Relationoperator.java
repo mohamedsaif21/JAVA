73af5e51440e5e73917a118cf4b03e30b593c9fe
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 11:32:15 +0530

    Relation operator

---
+++ Javacourse/Relationoperator.java
@@ -0,0 +1,21 @@
+package Javacourse;
+
+public class Relationoperator {
+
+    public static void main(String[] args) {
+        
+        int x =5;
+        int y =5;
+
+        boolean result = x < y;
+        boolean result1 = x > y;
+        boolean result2 = x != y;
+        boolean result3 = x == y;
+
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+    
+}
