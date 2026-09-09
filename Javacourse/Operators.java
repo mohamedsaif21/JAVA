537258aaaba36405f580eeb7d340f9d661625fc5
mohamedsaif21 <mohamedsaifb24@gmail.com>
2026-09-09 06:26:15 +0530

    Operators

---
+++ Javacourse/Operators.java
@@ -0,0 +1,42 @@
+package Javacourse;
+
+
+class Operators {
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 4;
+        int a = 7;
+
+
+        /*int add = num1 + num2;
+        int sub = num1 - num2;
+        int mul = num1 * num2;
+        int div = num1 / num2;
+        //Modulus operators
+        int modula = num1 % num2;*/
+
+         //a = a + 2;
+        /*a += 2;
+         a -= 2;
+         a *= 2;
+         a /= 2;
+         a %= 2; */
+
+         //a++;
+         a--;
+        
+    /*System.out.println("This are the operators");
+        System.out.println("Add : " + add);
+        System.out.println("sub: " + sub);
+        System.out.println("multiple: " + mul);
+        System.out.println("division: " + div);
+        System.out.println("MOdulus operators: " + modula);*/
+   
+        System.out.println(a);
+        
+        
+    }
+
+    
+}
